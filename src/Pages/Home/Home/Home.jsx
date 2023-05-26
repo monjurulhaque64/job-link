8d8121b5f4c9f5822b98aaf1385a95e8d4fbeec6
monjurulhaque64 <monjurulhoque64@gmail.com>
2023-05-26 19:46:22 +0600

    footer

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../Share/Navbar/Navbar';
+import Footer from '../../Share/Footer/Footer';
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Footer></Footer>
         </div>
     );
 };
